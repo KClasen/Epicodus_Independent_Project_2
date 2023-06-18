5fa9c7ddb5c7fb1f99a8a3211504a4de089c9b92
Kat Clasen <clasen.katherine@gmail.com>
2023-06-17 18:14:49 -0700

    fixed bugs. Runswithout error

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,58 @@
 
-   function handleSelector(event) {
-    event.preventDefault();
-    const num1 = parseInt(document.querySelector("input#input1").value);
-    const num2 = parseInt(document.querySelector("input#input2").value);
-    const num3 = parseInt(document.querySelector("input#input3").value);
-    const num4 = parseInt(document.querySelector("input#input4").value);
-    const num5 = parseInt(document.querySelector("input#input5").value);
+   // These are the scripts from the Mad Libs project.
+function handleSelector(event) {
+  event.preventDefault();
+  const num1 = parseInt(document.querySelector("input#input1").value);
+  const num2 = parseInt(document.querySelector("input#input2").value);
+  const num3 = parseInt(document.querySelector("input#input3").value);
+  const num4 = parseInt(document.querySelector("input#input4").value);
+  const num5 = parseInt(document.querySelector("input#input5").value);
    
   
-    let result1;
+  let result1;
     if (num1 <= num2 || num1 === num2) {
       result1 = "Ruby";
     } else if (num1 >= num3 || num1 === num3) {
       result1 = "C#";
     }
 
-    let result2;
+  let result2;
     if (num2 <= num3 || num2 === num3) {
-        result2 = "JavaScript";
+      result2 = "JavaScript";
     } else if (num2 >= num4 || num2 === num4) {
-        result2 = "Python";
+      result2 = "Python";
     } 
 
-    let result3;
+  let result3;
     if (num3 <= num4 || num3 === num4) {
-        result3 = "Swift";
+      result3 = "Swift";
     } else if (num3 >= num5 || num3 === num5) {
-        result3 = "Rust";
+      result3 = "Rust";
     } 
 
 
-    document.getElementById("output1").innerText = result1;
-    document.getElementById("output2").innerText = result2;
-    document.getElementById("output3").innerText = result3;
-  }
+  document.getElementById("output1").innerText = result1;
+  document.getElementById("output2").innerText = result2;
+  document.getElementById("output3").innerText = result3; 
+}
   
-  window.addEventListener("load", function() {
-    const form = document.getElementById("questions")
-    form.addEventListener("submit", handleSelector);
-    const resetBtn = document.getElementById("reset");
+window.addEventListener("load", function() {
+  const form = document.getElementById("questions")
+  form.addEventListener("submit", handleSelector);
+  const resetBtn = document.getElementById("reset");
     
-    form.addEventListener("submit", function() {
-        resetBtn.removeAttribute("class");
-      });
+  form.addEventListener("submit", function() {
+    resetBtn.removeAttribute("class");
+    });
 
-      resetBtn.addEventListener("click", function() {
-        document.querySelector("input#input1").value = null;
-        document.querySelector("input#input2").value = null;
-        document.querySelector("input#input3").value = null;
-        document.querySelector("input#input4").value = null;
-        document.querySelector("input#input5").value = null;
-        document.getElementById("output1").innerText = null;
-        document.getElementById("output2").innerText = null;
-        document.getElementById("output3").innerText = null;
-      });
-  });
-
+  resetBtn.addEventListener("click", function() {
+    document.querySelector("input#input1").value = null;
+    document.querySelector("input#input2").value = null;
+    document.querySelector("input#input3").value = null;
+    document.querySelector("input#input4").value = null;
+    document.querySelector("input#input5").value = null;
+    document.getElementById("output1").innerText = null;
+    document.getElementById("output2").innerText = null;
+    document.getElementById("output3").innerText = null;
+    });
+});
